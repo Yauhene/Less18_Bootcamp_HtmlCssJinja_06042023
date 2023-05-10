d80af9c6db3f3d91ee9631100cbaaf878696e797
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-11 00:49:30 +0300

    Уррраааа!!! Заработало!

---
+++ flask_bootcamp/flask_bootcamp/LoginForm.py
@@ -0,0 +1,12 @@
+﻿from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
+class Lf(FlaskForm): # форма логина
+    name = StringField("Имя", validators = [DataRequired()]) # имя
+    email = EmailField("Почта", validators = [DataRequired()]) # email
+    password = PasswordField("Пароль", validators = [DataRequired()]) # пароль 
+    password_again = PasswordField("Повторите пароль", validators = [DataRequired()]) # повтор пароля
+    remember_me = BooleanField("Запомнить меня")
+    submit = SubmitField("Зарегистрироваться")
+    
--- flask_bootcamp/flask_bootcamp/main3.py
+++ flask_bootcamp/flask_bootcamp/main3.py
@@ -1,21 +1,35 @@
 ﻿from flask import Flask, render_template
+from LoginForm import Lf
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello hello hello hello hello world'
 
 
 @app.route('/')
 @app.route('/index')
 def main():
-    with open('file.txt','r', encoding = 'utf-8') as file:
-        resultData = list()
-        for line in file.readlines():
-            resultData.append((tuple(line.split('\n')[0].split(';'))))
-            
-    return render_template('base3.html', data=resultData)
+    return render_template('base3.html')
 
-@app.route('/about')
-def about():
-    return render_template('/about.html')
+@app.route('/register', methods=['GET', 'POST'])
+def reg():
+    form = Lf()
+    
+    if form.validate_on_submit(): 
+        
+        if form.password_again.data != form.password.data:
+           
+            return render_template('register.html', title = 'Регистрация', form=form, 
+                                   message='Пароли не совпадают!!!')
+        
+        with open('file_2.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{form.name.data}; {form.email.data}; {form.password.data}; \n')
+
+        return render_template('register.html', message='Регистрация прошла успешно')
+    
+    return render_template('register.html', title = 'Регистрация', form=form)
+
+
+
 
 if __name__ == '__main__':
     app.run()
